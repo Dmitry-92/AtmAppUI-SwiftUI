889a8ea1192674964ddea5879467ec5ca85ad09c
Дмитрий М <s1ash@mail.ru>
2026-05-11 17:04:12 +0300

    Initial Commit

---
+++ AtmAppUI/AtmAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmAppUIApp.swift
+//  AtmAppUI
+//
+//  Created by Дмитрий on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct AtmAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
